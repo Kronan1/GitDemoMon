9a81eb088799c980155b8a3bdbc7525bcdfdcc96
Kronan1 <110824570+Kronan1@users.noreply.github.com>
2023-10-11 11:42:15 +0200

    First commit
    Added "hejsan världen".

---
+++ GitDemoMon/GitDemoMon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitDemoMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsan världen");
+        }
+    }
+}
